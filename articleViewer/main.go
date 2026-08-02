9f02aaf02622353853089bc35347b1285a502dd1
Stanley-0ps <ochaistanley7@gmail.com>
2026-08-02 11:52:57 +0100

    trying to write my handler

--- articleViewer/main.go
+++ articleViewer/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 )
 
 func ArticleHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		fmt.Fprint(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
 	idstr := r.URL.Query().Get("id")
-	Id, err := strconv.Atoi
+	Id, err := strconv.Atoi(idstr)
+	if err != nil {
+		fmt.Fprint(w, "bad request", http.StatusBadRequest)
+		return
+	}
 	for _, article := range articles {
 		if article.ID == Id {
 			templ.Execute.ParseFiles("article.html")
