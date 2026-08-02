8fd3d58ef52834c8d0e7b7b433d11f890cd58319
Stanley-0ps <ochaistanley7@gmail.com>
2026-08-02 13:50:07 +0100

    trying to write my handler

--- articleViewer/main.go
+++ articleViewer/main.go
@@ -17,10 +17,35 @@ func ArticleHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "bad request", http.StatusBadRequest)
 		return
 	}
-	templ, err := template.ParseFiles("article.html")
+	templ, err := template.ParseFiles("templates/article.html")
+	if err != nil {
+		fmt.Fprint(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
 
 	for _, article := range articles {
 		if article.ID == Id {
+			templ.Execute(w, article)
+			return
 		}
 	}
+}
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		fmt.Fprint(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	templ, err := template.ParseFiles("templates/home.html")
+	if err != nil {
+		fmt.Fprint(w, "internal server error", http.StatusInternalServerError)
+		fmt.Println(err)
+		return
+	}
+	templ.Execute(w, articles)
+
+}
+func main() {
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/article", ArticleHandler)
+	http.ListenAndServe(":8080", nil)
 }
