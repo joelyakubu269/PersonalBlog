daed7acb779e0d5e525e321b8982f06cc36a4a83
joyakubu <joelyakubu269@gmail.com>
2026-08-12 12:39:51 +0100

    working on the business logic

--- FileBasedArticle/service.go
+++ FileBasedArticle/service.go
@@ -4,26 +4,34 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 )
 
-func createAndStore(val ArticleData) {
-	id:= val.ID
-	new:= string(id)
-	file,err:= os.Create("article"+ new + ".json")
-	if err!= nil {
-		fmt.Errorf("unable to read file",err)
-		return
-	}
-	defer file.Close()
+func createAndStore(val ArticleData) error{
+	new := strconv.Itoa(val.ID)
+	file:="article"+ new + ".json"
+	
+	
 	data,err:= json.MarshalIndent(val,""," ")
 	if err!= nil {
-		fmt.Errorf("unable to marshall",err)
-		return
+	 
+	 return fmt.Errorf("unable to marshall",err)
+		
 	}
 	err = os.WriteFile(file,data,0644)
 	if err!= nil {
-		fmt.Errorf("unable to write to file",err)
-		return
+		
+		return fmt.Errorf("unable to write to file",err)
+	}
+	return nil
+}
+func delete(id int) error{
+	file:= fmt.Sprintf("article%d.json",id)
+	err:= os.Remove(file)
+	if err!= nil {
+		if errors.Is(err,Os.ErrNotExist) {
+			return fmt.Errorf()
+		}
 	}
 
 }
