3c71499a9e0e1e8ab6294dba2393ddfb25e684ed
joyakubu <joelyakubu269@gmail.com>
2026-08-12 10:08:17 +0100

    trying to create the business logic

--- FileBasedArticle/service.go
+++ FileBasedArticle/service.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"os"
 )
 
 func createAndStore(val ArticleData) {
-	id:= ArticleData.ID
-	file,err:=
+	id:= val.ID
+	new:= string(id)
+	file,err:= os.Create("article"+ new + ".json")
+	if err!= nil {
+		fmt.Errorf("unable to read file",err)
+		return
+	}
+	defer file.Close()
+	data,err= json.MarshalIndent(val,""," ")
+	if err!= nil {
+		fmt.Errorf("unable to marshall",err)
+		return
+	}
+	
 }
